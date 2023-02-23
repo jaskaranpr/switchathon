7a788ce9d8ec6f0a333158abcb4a7079c8541f8c
Jaskaran <jaskaran.s@coinswitch.co>
2023-02-23 18:44:47 +0530

    Fix: Layout

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,15 +1,16 @@
-import styled from "styled-components"
-
+import styled from "styled-components";
 
 const MainCon = styled.div`
-    border: 1px solid green;
-`
+  border: 1px solid green;
+`;
 const Home = () => {
   return (
-    <MainCon className="flex-1">
-
+    <MainCon className="flex-1 py-10 px-7 ">
+      <div className="bg-white py-2 px-3 w-[50%] rounded-full">
+        <input className="focus-within:outline-none" type="text" />
+      </div>
     </MainCon>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
